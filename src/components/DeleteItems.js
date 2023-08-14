5931540ced40cc1f27d535abdb62564033ffa425
Sandeep Kumar G R <sand.kumar.gr@gmail.com>
2023-08-15 01:53:44 +0530

    Added snackbar - basic implementation

--- src/components/DeleteItems.js
+++ src/components/DeleteItems.js
@@ -1,10 +1,14 @@
 /*global axios */
 
 import React, { useEffect, useState, useContext } from "react";
-import { Button } from "@mui/material";
+import { Button, SnackbarContent, Typography, Box } from "@mui/material";
+import Snackbar from "@mui/material/Snackbar";
 import DeleteIcon from "@mui/icons-material/DeleteRounded";
 import { deleteItemsURL, csrftokenURL } from "../config";
 import { ItemSelectionContext } from "./Context";
+import CircularProgress from "@mui/material/CircularProgress";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
 
 async function fetchCSRFToken(csrfurl) {
   const response = await fetch(csrfurl);
@@ -16,6 +20,19 @@ const DeleteItems = () => {
   const [CSRFToken, setCSRFToken] = useState("");
   const { fileIds, directories } = useContext(ItemSelectionContext);
   const [isDeleting, setIsDeleting] = useState(false);
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
   console.log("delete item rendered");
   useEffect(() => {
     fetchCSRFToken(csrftokenURL)
@@ -23,7 +40,7 @@ const DeleteItems = () => {
       .catch((err) => console.log(err));
   }, []);
 
-  const handleClick = (e) => {
+  const handleDelete = (e) => {
     e.preventDefault();
     e.stopPropagation();
     console.log(fileIds, directories);
@@ -53,22 +70,64 @@ const DeleteItems = () => {
       })();
     }
   }, [isDeleting]);
+  const action = (
+    <React.Fragment>
+      <Button color="secondary" size="small" onClick={handleClose}>
+        UNDO
+      </Button>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleClose}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </React.Fragment>
+  );
   return (
-    <Button
-      onClick={handleClick}
-      variant="outlined"
-      disableRipple
-      sx={{
-        border: "none",
-        boxSizing: "border-box",
-        "&:hover": {
-          backgroundColor: "#EFF3FA",
+    <>
+      <Button
+        onClick={handleDelete}
+        variant="outlined"
+        disableRipple
+        sx={{
           border: "none",
-        },
-      }}
-    >
-      <DeleteIcon color="primary" sx={{ cursor: "pointer", fontSize: 25 }} />
-    </Button>
+          boxSizing: "border-box",
+          "&:hover": {
+            backgroundColor: "#EFF3FA",
+            border: "none",
+          },
+        }}
+      >
+        <DeleteIcon color="primary" sx={{ cursor: "pointer", fontSize: 25 }} />
+      </Button>
+      {isDeleting && (
+        <Snackbar
+          open={true}
+          anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+          onClose={handleClick}
+        >
+          <SnackbarContent
+            message={
+              <Box
+                display="flex"
+                flexDirection="row"
+                alignItems="center"
+                justifyContent="flex-start"
+                gap={2}
+              >
+                <CircularProgress />
+                <Typography>
+                  Deleting {fileIds.length} files & {directories.length} folders
+                </Typography>
+              </Box>
+            }
+            action={action}
+          ></SnackbarContent>
+        </Snackbar>
+      )}
+    </>
   );
 };
 
