31dd162a85b67c688e762e0bf25bc24b5c1e64c7
Sandeep Kumar G R <sand.kumar.gr@gmail.com>
2023-08-17 10:36:31 +0530

    Added progress snackbar while download is happening

--- src/components/Download.js
+++ src/components/Download.js
@@ -1,10 +1,26 @@
 /* global axios */
 
 import React, { useState, useContext, useEffect } from "react";
-import { Button } from "@mui/material";
+import {
+  Button,
+  Box,
+  Stack,
+  Snackbar,
+  SnackbarContent,
+  Typography,
+} from "@mui/material";
 import CloudDownloadIcon from "@mui/icons-material/CloudDownloadRounded";
+import CircularProgress from "@mui/material/CircularProgress";
+import LinearProgress from "@mui/material/LinearProgress";
+
 import { ItemSelectionContext } from "./Context";
 import { downloadItemsURL, csrftokenURL } from "../config.js";
+
+import { styled } from "@mui/material/styles";
+
+const FullWidthLinearProgress = styled(LinearProgress)({
+  width: "100%",
+});
 
 async function fetchCSRFToken(csrfurl) {
   const response = await fetch(csrfurl);
@@ -16,6 +32,12 @@ const Download = () => {
   const { fileIds, directories } = useContext(ItemSelectionContext);
   const [CSRFToken, setCSRFToken] = useState("");
   const [startDownload, setStartDownload] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const handleClose = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
   const handleClick = (e) => {
     e.preventDefault();
     e.stopPropagation();
@@ -34,37 +56,84 @@ const Download = () => {
     ) {
       const headers = {
         "X-CSRF-Token": CSRFToken,
-        "Content-Type": "application/x-www-form-urlencoded",
+        "Content-Type": "application/json",
       };
       const body = {
-        files: JSON.stringify(fileIds),
-        directories: JSON.stringify(directories),
+        files: fileIds,
+        directories: directories,
       };
-      axios
-        .post(downloadItemsURL, body, { headers: headers })
-        .then((res) => console.log(res.data))
-        .catch((err) => console.log(err));
+      const options = {
+        method: "POST",
+        credentials: "include",
+        headers: headers,
+        body: JSON.stringify(body),
+      };
+
+      (async () => {
+        let rs_src = fetch(downloadItemsURL, options).then(async (response) => {
+          return response.body;
+        });
+
+        // create writable stream for file
+        let ws_dest = window.showSaveFilePicker().then((handle) => {
+          setOpen(true);
+          return handle.createWritable();
+        });
+
+        // create transform stream for decryption
+        let ts_dec = new TransformStream({
+          async transform(chunk, controller) {
+            controller.enqueue(chunk);
+          },
+        });
+        // stream cleartext to file
+        let rs_clear = rs_src.then((s) => s.pipeThrough(ts_dec));
+        return (await rs_clear).pipeTo(await ws_dest);
+      })().then(() => setOpen(false));
     }
   }, [startDownload, CSRFToken]);
   return (
-    <Button
-      variant="outlined"
-      disableRipple
-      sx={{
-        border: "none",
-        boxSizing: "border-box",
-        "&:hover": {
-          backgroundColor: "#EFF3FA",
+    <>
+      <Button
+        variant="outlined"
+        disableRipple
+        sx={{
           border: "none",
-        },
-      }}
-      onClick={handleClick}
-    >
-      <CloudDownloadIcon
-        color="primary"
-        sx={{ cursor: "pointer", fontSize: 25 }}
-      />
-    </Button>
+          boxSizing: "border-box",
+          "&:hover": {
+            backgroundColor: "#EFF3FA",
+            border: "none",
+          },
+        }}
+        onClick={handleClick}
+      >
+        <CloudDownloadIcon
+          color="primary"
+          sx={{ cursor: "pointer", fontSize: 25 }}
+        />
+      </Button>
+      <Snackbar
+        open={open}
+        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+        onClose={handleClose}
+      >
+        <SnackbarContent
+          message={
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "flex-between",
+                alignItems: "center",
+              }}
+            >
+              <CircularProgress />
+              <Typography>Downloading...</Typography>
+            </Box>
+          }
+        />
+      </Snackbar>
+    </>
   );
 };
 
