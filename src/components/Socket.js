bfcbc07f6fbc603f29f0a314c065e32b84c61ffc
Sandeep Kumar G R <sand.kumar.gr@gmail.com>
2024-04-13 21:32:48 +0530

    updated envs

--- src/components/Socket.js
+++ src/components/Socket.js
@@ -1,6 +1,5 @@
 import { io } from "socket.io-client";
 import { server } from "../config.js";
 
-// const URL = process.env.REACT_APP_ENV === "production" ? undefined : server;
 console.log(server);
 export const socket = io(server, { autoConnect: false });
