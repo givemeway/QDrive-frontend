a2fc7f7e4337a7bfa5fca86b9b2cf9fb73a20238
Sandeep Kumar G R <sand.kumar.gr@gmail.com>
2024-04-13 14:05:21 +0530

    changed redux thunk base url to backend

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -2,7 +2,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "/app" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://backup-8toq.onrender.com/app",
+  }),
   endpoints: (builder) => ({
     deleteTrashItems: builder.mutation({
       query: (data) => ({
