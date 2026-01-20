d6f349bd8e42d0dbe9bf6f3282b40b215de378d8
Sandeep Kumar G R <sand.kumar.gr@gmail.com>
2026-01-20 19:10:43 +0530

    added websocket api url evn into the app

--- src/components/Socket.js
+++ src/components/Socket.js
@@ -3,7 +3,7 @@ import { PRODUCTION } from "../config.js";
 
 const server =
   process.env.REACT_APP_ENV === PRODUCTION
-    ? process.env.REACT_APP_BASE_API_URL
+    ? process.env.REACT_APP_SOCKET_API_URL
     : "http://localhost:3005";
 console.log("Socket Server-->", server);
 export const socket = io(server, { autoConnect: false });
